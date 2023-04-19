a029cf0fc3741991b1be4f4069a65786a74adce7
Polina Kasich <poli.key@yandex.ru>
2023-04-19 08:32:15 +0500

    1 and 2 tasks done

---
+++ 1.py
@@ -0,0 +1,6 @@
+day = 19
+month = "апреля"
+temperature = 7
+print(f"Сегодня {day} {month}. На улице {temperature} градусов.")
+if (temperature < 0):
+    print("Холодно, лучше остаться дома")
---
+++ 2.py
@@ -0,0 +1,3 @@
+speed_kmh = 1079252848.8
+speed_kms = speed_kmh / 3600
+print(f"Скорость света равна {int(speed_kms)} км/с")
